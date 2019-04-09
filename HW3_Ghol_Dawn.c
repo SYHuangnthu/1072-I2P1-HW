1bfa5644e26ccf8020de398d130a1bc314583648
binbinko <43368507+binbinko@users.noreply.github.com>
2019-04-09 21:46:41 +0800

    Update HW3_Ghol_Dawn.c

--- HW3_Ghol_Dawn.c
+++ HW3_Ghol_Dawn.c
@@ -5,20 +5,21 @@ int main(void)
     int n,k,i,j;
     long long int qq;
     long long int a[5010] = {0};//³Ì¦h5000
-    double x=0, temp=0,b=1.0;
+    double x=0, temp=0;
     scanf("%d %d\n", &n,&k);
     for(int flag=1; flag<=n; flag++)
     {
         scanf("%lld", &qq);
         a[flag]= a[flag-1]+qq;
     }
-    for(i=1;i<=n;i++)
+    for(i=1;i<=n-k+1;i++)
     {
-        for(j=i+1;j<=n;j++)
+        for(j=i+k-1;j<=n;j++)
         {
-            temp = (a[j]-a[i-1]);
+            temp = (double)(a[j]-a[i-1]);
             temp = temp/(j-i+1);
 //            printf("%.3lf\n", temp);
+
             if(temp>x)
                 x=temp;
         }
